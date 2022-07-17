bb730b4deb65e3a3b6a131252454c7485b13956f
Pranayr49741 <96970138+Pranayr49741@users.noreply.github.com>
2022-07-17 13:46:17 +0530

    Update config.py

--- Shikimori/config.py
+++ Shikimori/config.py
@@ -16,17 +16,17 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1606221784  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "SOME1HING"
+    API_ID = 3716422  # integer value, dont use ""
+    API_HASH = "779c90c42677827af815808dac47244c"
+    TOKEN = "5565096928:AAG5ZV21qcBm5BGRDHLIV_9HlEBba71o1Ok"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 1208988512  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "@bunny_2021"
     SUPPORT_CHAT = "tyranteyeeee"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001432609692
+        -1001506824512
     )  # Prints any new group the bot is added to, prints just the name and ID.
     LOG_CHANNEL = (
-        -1001150905176
+        -1528547826
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
